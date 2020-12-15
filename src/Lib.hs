6ae99c64377461b8c3b60cbbe368e0f22d3290d0
Kian Fay <kifay@tcd.ie>
2020-12-15 13:15:07 +0000

    Added username as a parameter

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,27 +7,27 @@ module Lib
     ( someFunc
     ) where
 
-import GithubRepresent
+import GithubRepresent ( getUser )
 import Servant.Client
 import Network.HTTP.Client          (newManager)
 import Network.HTTP.Client.TLS      (tlsManagerSettings)
+import Data.Text ( Text )
 
 someFunc :: IO ()
-someFunc = doGithubReq
+someFunc = doGithubReq "kianfay"
 
-doGithubReq :: IO()
-doGithubReq =   let env :: IO Servant.Client.ClientEnv
-                    env = do
-                        manager <- newManager tlsManagerSettings
-                        
-                        -- Here we define a ClientEnv smart using the smart constructor, mkClientEnv
-                        return $ Servant.Client.mkClientEnv manager (Servant.Client.BaseUrl {baseUrlScheme = Servant.Client.Http, baseUrlHost = "api.github.com", baseUrlPort = 80, baseUrlPath = ""})
-                -- m >>= k suggests "feed the result of computation m to the function k" - from StackOverflow
-                -- We could also put case after in, and we would not write case with a \.
-                in (Servant.Client.runClientM (GithubRepresent.getUser (Just "Visualisation-App") "kianfay") =<< env) >>= \case
-                    Left err -> do
-                        -- The show function is passed an instance of the class Show (err or res in this case, being an 
-                        -- instance of ClientError or Data.Text.Internal.Text) and returns a string representation
-                        putStrLn ("We have an ERROR -- " ++ show err)
-                    Right res -> do
-                        putStrLn ("We made a successful request and got the response -- " ++ show res)
+doGithubReq :: Text -> IO()
+doGithubReq username =  let env :: IO Servant.Client.ClientEnv
+                            env = do
+                                manager <- newManager tlsManagerSettings
+                                -- Here we define a ClientEnv smart using the smart constructor, mkClientEnv
+                                return $ Servant.Client.mkClientEnv manager (Servant.Client.BaseUrl {baseUrlScheme = Servant.Client.Http, baseUrlHost = "api.github.com", baseUrlPort = 80, baseUrlPath = ""})
+                        -- m >>= k suggests "feed the result of computation m to the function k" - from StackOverflow
+                        -- We could also put case after in, and we would not write case with a \.
+                        in (Servant.Client.runClientM (GithubRepresent.getUser (Just "Visualisation-App") username) =<< env) >>= \case
+                            Left err -> do
+                                -- The show function is passed an instance of the class Show (err or res in this case, being an 
+                                -- instance of ClientError or Data.Text.Internal.Text) and returns a string representation
+                                putStrLn ("We have an ERROR -- " ++ show err)
+                            Right res -> do
+                                putStrLn ("We made a successful request and got the response -- " ++ show res)
