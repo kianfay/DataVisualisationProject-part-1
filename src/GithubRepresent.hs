d30a18793fceab2481723823341dd6604ec42186
Kian Fay <kifay@tcd.ie>
2020-12-27 19:39:06 +0000

    First two GETS for final project implemented

--- src/GithubRepresent.hs
+++ src/GithubRepresent.hs
@@ -3,31 +3,52 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DuplicateRecordFields     #-}
 
 module GithubRepresent where 
 
-import Control.Monad       (mzero)
-import Data.Aeson
-import Data.Proxy
-import Data.Text
-import GHC.Generics
-import Servant.API
-import Servant.Client
+import           Control.Monad       (mzero)
+import           Data.Aeson
+import           Data.Proxy
+import           Data.Text
+import           GHC.Generics
+import           Network.HTTP.Client (defaultManagerSettings, newManager)
+import           Servant.API
+import           Servant.Client
 
-data User = User {
-    name :: String
+data Org = Org {
+    public_repos :: Int
+} deriving (Generic, FromJSON, Show)
+
+data Repo = Repo {
+    name :: Text
 } deriving (Generic, FromJSON, Show)
 
 -- Here we define our API, outlining the structure of our HTTP requests in a abstracted way
-type GitHubAPI = "users" :> Header "User-Agent" Text
-                         :> Capture "username" Text 
-                         :> Get '[JSON] User
+-- The first one gets the Organization endpoint of a given organization, so we can get the number of public repos.
+-- The second one gets the repos of a given organization.
+type GitHubAPI =     Header "User-Agent" Text
+                    :> "orgs"
+                    :> Capture "organization" Text 
+                    :> Get '[JSON] Org
+                :<|> Header "User-Agent" Text
+                    :> "orgs"
+                    :> Capture "organization" Text 
+                    :> "repos"
+                    :> QueryParam "per_page" Int
+                    :> Get '[JSON] [Repo]
 
 -- We associate our type with this proxy type variable so that we can pass the type to Servant
 gitHubAPI :: Proxy GitHubAPI
 gitHubAPI = Proxy
 
--- Takes text(the username) and returns ClientM Text 
--- We import this function from this class into Lib.hs, and pass it to the Servant module
-getUser :: Maybe Text -> Text -> ClientM User  
-getUser = client gitHubAPI
+-- Define the types of the different api functions (to translate gitHubAPI to Haskell functions)
+type Organization = Text
+type UserAgentHeader = Text
+type PerPage = Int
+
+getOrg :: Maybe UserAgentHeader -> Organization -> ClientM Org 
+
+getRepos :: Maybe UserAgentHeader -> Organization -> Maybe PerPage -> ClientM [Repo]
+
+getOrg :<|> getRepos = client gitHubAPI
--- src/Lib.hs
+++ src/Lib.hs
@@ -9,30 +9,37 @@ module Lib
     ( someFunc
     ) where
 
-import GithubRepresent ( getUser )
+import GithubRepresent
 import Servant.Client
 import Network.HTTP.Client          (newManager)
 import Network.HTTP.Client.TLS      (tlsManagerSettings)
-import Data.Text ( Text )
+import Data.Text hiding (map,intercalate)
+import Data.List (intercalate)
 
 -- This is the function called by Main.hs
 someFunc :: IO ()
-someFunc = doGithubReq "kianfay"
+someFunc = doGithubReq
 
--- This function accepts a useranme of type Text, and performs
--- the Servant functionality
-doGithubReq :: Text -> IO()
-doGithubReq username =  let env :: IO Servant.Client.ClientEnv
-                            env = do
+-- A separate function which gets the data from the API endpoints
+queries :: ClientM (Org, [Repo])
+queries = do
+    org <-  getOrg (Just "Visualisation-App") "microsoft"
+    repo <- getRepos (Just "Visualisation-App") "microsoft" (Just 1)
+    return  (org, repo)
+
+-- This function now essentially runs the Queries function and prints the output 
+doGithubReq :: IO()
+doGithubReq =   let env :: IO Servant.Client.ClientEnv
+                    env = do
                                 manager <- newManager tlsManagerSettings
                                 -- Here we define a ClientEnv smart using the smart constructor, mkClientEnv
                                 return $ Servant.Client.mkClientEnv manager (Servant.Client.BaseUrl {baseUrlScheme = Servant.Client.Http, baseUrlHost = "api.github.com", baseUrlPort = 80, baseUrlPath = ""})
                         -- m >>= k suggests "feed the result of computation m to the function k" - from StackOverflow
                         -- We could also put case after in, and we would not write case with a \.
-                        in (Servant.Client.runClientM (GithubRepresent.getUser (Just "Visualisation-App") username) =<< env) >>= \case
+                in (Servant.Client.runClientM (queries) =<< env) >>= \case
                             Left err -> do
                                 -- The show function is passed an instance of the class Show (err or res in this case, being an 
                                 -- instance of ClientError or Data.Text.Internal.Text) and returns a string representation
                                 putStrLn ("We have an ERROR -- " ++ show err)
-                            Right res -> do
-                                putStrLn ("We made a successful request and got the response -- " ++ show res)
+                            Right (org, repo) -> do
+                                putStrLn $ show org ++ show repo
